a04167cde374cecc2ab4701dbd5e04d53c56fcd8
Deivis Dany Jimenez Hernandez <DeivisJimenez@comfama.com.co>
2022-12-08 15:49:37 -0500

    Adelanto del proycto menú

--- creandomenu.py
+++ creandomenu.py
@@ -1,23 +1,37 @@
+
+from ast import Num
+from ntpath import join
+
+
 opcion = 100
 
 print("Empanadas El machetico")
+print("Elija, por favor, una de las siguientes opciones:")
 print("***********************\n")
 
 print("1. Registrar empanada ")
 print("2. Ver empanada empanada ")
 print("0. Salir ")
 
+
 empanadas = []
+
+
 
 while opcion != 0:
     opcion = int(input("Digita una opción: "))
     if opcion == 1:
-        empanada = input("Digite el nombre de la empanada que quiere registrar: ")
+        empanada = input("Digite la(s) empanada(s) que desea registrar: ")
         empanadas.append(empanada)
     elif opcion == 2:
+        print("PEDIDO:")
         for empanada in empanadas:
-            print(f"La empanada que pediste es {empanada}")
+            if len(empanadas) > 1:
+                print(f"Empanada de {empanada}")
+            else:
+                print(f"Usted pidió una empanada de {empanada}")
     elif opcion == 0:
         break
     else:
         print("Apreciado usuario, debe seleccionar  una opción válida.")
+
